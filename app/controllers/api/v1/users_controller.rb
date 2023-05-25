2747cf192eacbebe91c1b5896ccc16a99e8ed1e3
Rivelino Junior <rv.fernandes.junior@gmail.com>
2023-05-25 13:46:48 -0300

    create endpoint POST /api/v1/users to create a common user

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def create
+        response = ::Users::CreateCommonUser.new(user_params).call
+
+        if response.success
+          render json: { user: response.data }, status: :created
+        else
+          render json: { errors: response.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:full_name, :user_name, :email).to_h.deep_symbolize_keys
+      end
+    end
+  end
+end
---
+++ spec/requests/api/v1/post_users_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'POST /api/v1/users' do
+  subject(:perform_request) { post '/api/v1/users', params: user_params }
+
+  context 'when user creation is successful' do
+    let(:user_params) { { user: attributes_for(:user) } }
+
+    it 'returns a success response with the created user' do
+      perform_request
+
+      user_created = Users::Query.last
+
+      expect(response.body).to eq({ user: user_created }.to_json)
+    end
+
+    it do
+      perform_request
+
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  context 'when user creation fails' do
+    let(:user_params) { { user: attributes_for(:user, full_name: '') } }
+
+    it do
+      perform_request
+
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+
+    it 'returns an error response with the validation errors' do
+      perform_request
+
+      expect(response.body).to eq({ errors: ["Full name can't be blank"] }.to_json)
+    end
+  end
+end
